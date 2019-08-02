69d58243bf9d98cc3b1536b84caec44016ca5035
Jack McKay <jackandrewmckay@gmail.com>
2019-08-02 20:18:10 +0100

    #2 - Implemented AgeString VR.

---
+++ Data/ValueRepresentations/AgeString.cs
@@ -0,0 +1,9 @@
+namespace Osmosys.Data.ValueRepresentations
+{
+    public class AgeString : MultiStringElement
+    {
+        public AgeString(DicomTag tag, string value) : base(tag, value)
+        {
+        }
+    }
+}
