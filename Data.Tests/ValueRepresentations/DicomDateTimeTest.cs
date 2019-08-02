762fade3aa54fbcbe3d4c1a2fc225f6ea66af293
Jack McKay <jackandrewmckay@gmail.com>
2019-08-03 00:59:53 +0100

    #7 - Added Date Time VR.

---
+++ Data.Tests/ValueRepresentations/DicomDateTimeTest.cs
@@ -0,0 +1,88 @@
+using System;
+using Osmosys.Data.ValueRepresentations;
+using Xunit;
+
+namespace Osmosys.Data.Tests.ValueRepresentations
+{
+    public class DicomDateTimeTest
+    {
+        [Fact]
+        public void CanGetDate()
+        {
+            var dateA = new DateTime(2001, 09, 30);
+            var dateB = new DateTime(2002, 09, 29);
+            var tag = new DicomTag(2, 1);
+            var date = new DicomDateTime(tag, new[] {dateA, dateB});
+
+            var value = date.GetDate(1);
+
+            Assert.Equal(dateB, value);
+        }
+
+        [Fact]
+        public void ThrowsOnInvalidDateRead()
+        {
+            var dateA = new DateTime(2001, 09, 30);
+            var tag = new DicomTag(2, 1);
+            var date = new DicomDateTime(tag, dateA);
+
+            Assert.Throws<ArgumentException>(() => date.GetDate(1));
+        }
+
+        [Fact]
+        public void CanGetDates()
+        {
+            var dates = new[] {new DateTime(2001, 09, 30)};
+            var tag = new DicomTag(2, 1);
+            var date = new DicomDateTime(tag, dates);
+
+            var values = date.GetDates();
+
+            Assert.Equal(dates, values);
+        }
+
+        [Fact]
+        public void CanUpdateDate()
+        {
+            var tag = new DicomTag(2, 1);
+            var date = new DicomDateTime(tag, null);
+            var expected = DateTime.Now;
+            date.Update(expected);
+            var actual = date.GetDate(0);
+            Assert.Equal(expected, actual);
+        }
+
+        [Fact]
+        public void CanUpdateDates()
+        {
+            var tag = new DicomTag(2, 1);
+            var expected = new[] {DateTime.Now, DateTime.Now.AddDays(1)};
+            var date = new DicomDateTime(tag, null);
+            date.Update(expected);
+            var actual = date.GetDates();
+            Assert.Equal(expected, actual);
+        }
+
+        [Fact]
+        public void ReplacesNullWithEmptyArray()
+        {
+            var tag = new DicomTag(2, 1);
+            var date = new DicomDateTime(tag, null);
+            date.Update(null as DateTime[]);
+            var actual = date.GetDates();
+            Assert.Empty(actual);
+        }
+
+
+        [Fact]
+        public void ThrowsOnStringRead()
+        {
+            var expected = new DateTime(2019, 03, 21);
+            var tag = new DicomTag(2, 1);
+            var date = new DicomDateTime(tag, expected);
+
+            Assert.Throws<InvalidCastException>(() => date.GetString(0));
+            Assert.Throws<InvalidCastException>(() => date.GetStrings());
+        }
+    }
+}
---
+++ Data/ValueRepresentations/DicomDateTime.cs
@@ -0,0 +1,41 @@
+using System;
+
+namespace Osmosys.Data.ValueRepresentations
+{
+    public class DicomDateTime : DicomElement
+    {
+        private DateTime[] _values;
+
+        public DicomDateTime(DicomTag tag, DateTime value) : base(tag)
+        {
+            _values = new[] {value};
+        }
+
+        public DicomDateTime(DicomTag tag, DateTime[] values) : base(tag)
+        {
+            _values = values ?? Array.Empty<DateTime>();
+        }
+
+        public override void Update(DateTime date)
+        {
+            _values = new[] {date};
+        }
+
+        public override void Update(DateTime[] dates)
+        {
+            _values = dates ?? Array.Empty<DateTime>();
+        }
+
+        public override DateTime GetDate(int index)
+        {
+            if (index < 0 || index >= _values.Length)
+            {
+                throw new ArgumentException($"Index {index} does not exist.");
+            }
+
+            return _values[index];
+        }
+
+        public override DateTime[] GetDates() => _values;
+    }
+}
